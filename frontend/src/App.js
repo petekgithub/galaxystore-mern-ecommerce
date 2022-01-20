ac0f3c289ad7db77526f7eb07614b2c3ef4a3311
petekgithub <petekshamitbeyli@gmail.com>
2022-01-20 22:09:36 +0300

    Admin Product List

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,8 +15,7 @@ import PlaceOrderScreen from './screens/PlaceOrderScreen'
 import OrderScreen from './screens/OrderScreen'
 import UserListScreen from './screens/UserListScreen'
 import UserEditScreen from './screens/UserEditScreen'
-
-
+import ProductListScreen from './screens/ProductListScreen'
 
 
 
@@ -43,8 +42,9 @@ const App = () => {
           <Route path='/product/:id' element={<ProductScreen/>}/>
           {/* <Route path='/cart/:id?' element={<CartScreen/>}/> */}
           <Route path="/cart/:id" element={<CartScreen />} />
-          <Route path="/admin/userList" element={<UserListScreen />} />
+          <Route path="/admin/userlist" element={<UserListScreen />} /> 
           <Route path="/admin/user/:id/edit" element={<UserEditScreen />} />
+          <Route path="/admin/productlist" element={<ProductListScreen />} />
           <Route path="/cart" element={<CartScreen />} />
         </Routes>
 
